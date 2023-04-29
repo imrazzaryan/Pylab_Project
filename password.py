1317467ba758a07463964b645408fa6651eb1eee
imrazzaryan <131935546+imrazzaryan@users.noreply.github.com>
2023-04-29 17:15:50 +0530

    Password generator in python.

---
+++ password.py
@@ -0,0 +1,10 @@
+import random
+
+
+def generate():
+    n = random.randint(8, 12)
+    l = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*'
+    return ''.join(random.choices(l, k=n))
+
+
+print("The generated OTP is ", generate())
